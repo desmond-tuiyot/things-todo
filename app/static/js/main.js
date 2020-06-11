376f3500e2985100de24adf2ab1c09f8878371c6
Desmond Tuiyot <dtuiyot@gmail.com>
2020-06-11 19:14:40 -0400

    Functionality to allow user to add a task to the list when enter is pressed.

---
+++ app/static/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $(".todo-input").keypress(function(e){
+        let pressedKey = e.which || e.keyCode;
+        if (pressedKey===13){
+            let nextTask = $(this).val();
+            $(this).val("");
+            if (nextTask===""){
+                return false;
+            } else {
+                let taskHTML = "<li class=\"list-group-item todo-task\">\n" +
+                                "<label class=\"todo-label\">\n" +
+                                "<input type=\"checkbox\">\n" +
+                                "<span>" +nextTask+ "</span>" +
+                                "</label>\n" +
+                                "<button class=\"todo-quit\"><i class=\"fa fa-minus-circle\"></i></button>\n" +
+                                "</li>";
+                $(".todo-task-list").append(taskHTML);
+                $(this).val("")
+            }
+        }
+    });
+})
