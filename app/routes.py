3dd792ca12dc22b2d68948b5a6ba8dfe9922a6fb
Desmond Tuiyot <dtuiyot@gmail.com>
2020-06-15 20:11:48 -0400

    created log in page + login view, as well as basic navbar.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, flash
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def home():
-    return render_template('todo_layout.html')
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.email.data, form.remember.data))
+        return redirect(url_for('home'))
+    return render_template("login.html", form=form)
