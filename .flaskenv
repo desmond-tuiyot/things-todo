3dab0b42c02adccb1b4850807e3c861eef3aa49b
Desmond Tuiyot <dtuiyot@gmail.com>
2020-06-16 15:11:20 -0400

    tried (and failed) to set up error sending by email. Will fix later

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,10 @@
 APP_SETTINGS=config.DevelopmentConfig
+FLASK_DEBUG=0
 FLASK_APP=todo_app.py
-FLASK_DEBUG=1
 DATABASE_URL=postgresql://postgres:SHOP###dez7228@localhost/todo_app
+
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=nynetray@gmail.com
+MAIL_PASSWORD=SHOP@dez
--- config.py
+++ config.py
@@ -9,6 +9,13 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET KEY') or 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # email support - to notify admin of any errors
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['dtuiyot@gmail.com']
 
 
 class ProductionConfig(Config):
